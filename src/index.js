abcda2513824c0826a02e83d0b363a1466c14e61
rstallingsiii <59840429+rstallingsiii@users.noreply.github.com>
2022-10-24 21:28:33 -0500

    Finished code infrastructure. Need to clean errors

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import './index.css';
+import Game from './containers/Game';
+import reducer from './reducers';
+import registerServiceWorker from './registerServiceWorker';
+import './reportWebVitals.js';
+
+/* eslint-disable no-underscore-dangle */
+
+const store = createStore(
+  reducer, /* preloadedState */ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+/* eslint-enable */
+
+ReactDOM.render(
+  <Provider store={store}>
+    
+    <Game />
+
+  </Provider>, 
+
+  document.getElementById('root'), 
+);
+
+registerServiceWorker();
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals(console.log);
